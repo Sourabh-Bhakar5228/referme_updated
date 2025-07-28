6b7774fcb7ba809487fc9ffea3b2ccf4654b6ef0
Sourabh-Bhakar5228 <golubhakar5228@gmail.com>
2025-07-28 11:54:06 +0530

    solve login logout error again

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -20,7 +20,7 @@ import WhatWeDo from "../pages/About/WhatWeDo";
 import CoreComitee from "../pages/About/CoreComitee";
 import Freelancing from "../pages/Services/Freelancing";
 import PartnershipPrograms from "../pages/Services/PartnershipPrograms";
-import AdminLogin from "../admin/admin_Login_page/AdminLogin";
+import AdminLogin from "../admin/admin_Login_page/AdLogin";
 // import SignUp from "../pages/Signup";
 import AdminLayout from "../layout/AdminLayout";
 import Dashboard from "../admin/dashboard/Dashboard";
